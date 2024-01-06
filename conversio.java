edd8934c28c7aa5e1530f3aedbaa900a2af2c306
patriziatrudu <patriziamanuela.tru.dam@insalfonscostafreda.cat>
2024-01-06 11:20:43 +0100

    conversio_llires

--- conversio.java
+++ conversio.java
@@ -1,31 +1,18 @@
+package com.mycompany.conversio;
 
 import java.util.Scanner;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author patri
- package com.mycompany.conversio;
-
-import java.util.Scanner;
-
-/**
- *
- * @author nil
- */
 public class conversio {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        System.out.println("Introdueix en €");
-        int valor_euros = sc.nextInt();
-                
-        double tipus_de_canvi = 1.15d;
-        System.out.println(valor_euros + " eur0s (€) acatualmentt ewuivalen a" + (valor_euros * tipus_de_canvi) + " lliures esterlines (£)");
+
+        System.out.println("Introdueix en £");
+        double valorLliures = sc.nextDouble();
+
+        double tipusDeCanvi = 1.15d;
+        double valorEuros = valorLliures / tipusDeCanvi;
+
+        System.out.println(valorLliures + " lliures esterlines (£) actualment equivalen a " + valorEuros + " euros (€)");
     }
 }
